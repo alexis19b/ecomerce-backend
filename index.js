dbfacff10a37f9d7854899cbf9cdd9093399fa68
Jesus Alexis <ingjesusalexis@gmail.com>
2021-11-05 12:42:13 -0400

    inicio de proyecto

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(()=>console.log("BD Conexion exitosa")).catch((err)=>console.log(err));
 
-app.listen(5000, () => {
+app.use("/api/user", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
     console.log(`Server started on port 5000`);
 });
