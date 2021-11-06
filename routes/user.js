b35e73c56d47c85905bfd690036853b92073d111
Jesus Alexis <ingjesusalexis@gmail.com>
2021-11-06 16:52:45 -0400

    added get user all

--- routes/user.js
+++ routes/user.js
@@ -43,7 +43,22 @@ router.get("/find/:id", verifyTokenAndAdmin, async(req, res)=>{
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+//Get All Users
+
+router.get("/", verifyTokenAndAdmin, async(req, res)=>{
+    const query = req.query.new;
+    try {
+        const users = query 
+            ? await User.find().sort({ _id: -1}).limit(5);
+            : await User.find();
+        
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 
