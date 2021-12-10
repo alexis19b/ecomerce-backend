d3ca79448ec2ebccee153f5c738a3913bfe82d14
Jesus Alexis <ingjesusalexis@gmail.com>
2021-12-10 10:47:23 -0400

    added route products

--- routes/product.js
+++ routes/product.js
@@ -1,4 +1,84 @@
 const router = require("express").Router();
+const Product = require("../models/Product");
+const {
+  verifyToken,
+  verifyTokenAndAdmin,
+  verifyTokenAndAuthorization,
+} = require("./verifyToken");
 
+//Create
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
 
-module.exports= router;
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Updated
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updateProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    if (!updateProduct) {
+      return res.status(404).json("Producto no existe por el id");
+    }
+    res.status(200).json(updateProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Delete
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Producto ha sido Eliminado");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Get Product
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json("Producto no existe por el id");
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+//get all users
+router.get("/", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
+    //Lista los productos nuevos por fecha
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(1);
+      // si la query es category busca en el array la categoria
+    } else if (qCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
+    res.status(200).json(products);
+  } catch (error) {}
+});
+
+module.exports = router;
